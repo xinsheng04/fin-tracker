602849b10f5a70ad0e605eda378d0ea6aeeb0db6
Kyle <kyle.boudville@gmail.com>
2025-09-11 23:50:52 +0800

    added react-router, routing has been added to achieve SPA and sideBar component and button component has been added, with some type declarations

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import Button from './components/Button';
-
+import Sidebar from './components/Sidebar';
+import {useState} from 'react'; 
 
 function App() {
+  const [count, setCount] = useState<number>(0)
+  function handleClick (){ 
+    return "hello World"
+  }
+
+  function handleDecrease(){ 
+    setCount(prev=>{
+      return prev-1;
+    })
+  }
   return (
     <>
-    <Button>Hello World !</Button>
-      <p>Hello World</p>
+    <Sidebar onClick={handleClick}>Finance Tracker</Sidebar>
     </>
   )
 }
