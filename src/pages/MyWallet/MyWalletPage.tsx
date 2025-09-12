8787568cb41bf11e885d307efa4d5133ec01d7a3
Kyle <kyle.boudville@gmail.com>
2025-09-12 14:27:28 +0800

    wallet form has been initialized but not yet hooked up to the store

--- src/pages/MyWallet/MyWalletPage.tsx
+++ src/pages/MyWallet/MyWalletPage.tsx
@@ -1,24 +1,33 @@
 import Button from "../../components/button/Button"
-import {useState} from 'react';
+import { useState } from 'react';
+import Input from "../../components/input/Input";
+import styles from './myWallet.module.css';
 import { useDispatch } from "react-redux";
 import { addNewCard } from "../../store/myWallet";
 
 const MyWallet = () => {
-  const [addCard,setAddCard]= useState<boolean>(false);
+  const [addCard, setAddCard] = useState<boolean>(false);
 
-  function handleClickAddCard (){
+  function handleClickAddCard() {
     setAddCard(!addCard);
   }
   return (
-    <div>
+    <div className={styles.main}>
       <h1>My Wallet</h1>
-      <Button onClick={handleClickAddCard}>Add Card</Button>
-      {addCard &&
-      <form>
-        
-      </form>
-      
-      }
+      <div>
+        <Button onClick={handleClickAddCard}>Add Card</Button>
+      </div>
+      <div>
+        {addCard &&
+          <form action="">
+            <Input label="Card Number" name="card" type="number" required/>
+            <Input label="Bank Name" name="bankName" type="text" required></Input>
+            <Input label="Card Amount" name="amount" type="number" required></Input> 
+          </form>
+
+        }
+      </div>
+
     </div>
 
   )
