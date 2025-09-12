5f4a3918ca3c3bd62d538fa53675d09acb2cdccf
Kyle <kyle.boudville@gmail.com>
2025-09-12 15:42:14 +0800

    useSelector has been implement when user adds their wallet

--- src/pages/Overview/OverviewPage.tsx
+++ src/pages/Overview/OverviewPage.tsx
@@ -1,9 +1,20 @@
 import Header from "../../components/header/Header"
-const Overview: React.FC = () => {
+import { useSelector } from "react-redux";
+const Overview = () => {
+  // BankAccount is an array
+  const bankAccount = useSelector((state:any)=>state.myWallet.bankAccounts)
+  console.log(bankAccount);
   return (
     <>
       <Header />
-      
+      {bankAccount.map((account:any, idx:number)=>{
+        return(
+        <ul key={idx}>
+          <li>Bank name :{account.bankName}</li>
+          <li>Card Number : {account.cardNo}</li>
+          <li>Amount : {account.amount}</li>
+        </ul>
+      )})}
     </>
   );
 }
