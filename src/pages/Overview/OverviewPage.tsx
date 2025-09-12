96a83ae0b71269e45beffe80135c98b171a86425
xinsheng04 <freddy161204@gmail.com>
2025-09-12 16:17:21 +0800

    Created the balance cards for overview page

--- src/pages/Overview/OverviewPage.tsx
+++ src/pages/Overview/OverviewPage.tsx
@@ -1,7 +1,26 @@
 import Header from "../../components/header/Header"
+import CashBalance from "../../components/cashBalance/CashBalance";
+import styles from "./OverviewPage.module.css";
+import Button from "../../components/button/Button";
 const Overview: React.FC = () => {
   return (
-    <Header />
+    <div className={styles.container}>
+      <Header />
+      <div className={styles.balances}>
+        <CashBalance title="My Balance">
+          <div className={styles.balanceActions}>
+            <Button onClick={() => console.log("Add Income")}>
+              Transfer
+            </Button>
+            <Button onClick={() => console.log("Add Expense")}>
+              Received
+            </Button>
+          </div>
+        </CashBalance>
+        <CashBalance title="Monthly Income" />
+        <CashBalance title="Monthly Expenses" />
+      </div>
+    </div>
   );
 }
 
