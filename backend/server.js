9681783cb4c33f647ee1dbc892e6f1cc7e65ef7b
Kyle <kyle.boudville@gmail.com>
2025-09-26 11:24:04 +0800

    setup the backend

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get('/api',(req,res)=>{
+  res.json({"users":["userOne","userTwo"]})
+})
+
+app.listen(5000,()=>{
+  console.log('server started on port 5000')
+})
