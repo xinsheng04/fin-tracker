8f2c2ea92bb64088772339e3f92896581aa72b13
Kyle <kyle.boudville@gmail.com>
2025-10-01 13:06:10 +0800

    setup tanStack query and the Api folder

---
+++ src/api/Api.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers:{'Content-Type':'application/json'},
+  timeout:10000,
+})
+
+export default api;
--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,16 @@ import { RouterProvider } from 'react-router-dom'
 import router from './routes/routes';
 import { Provider } from 'react-redux';
 import store from './store';
+import { QueryClientProvider } from '@tanstack/react-query';
+import  queryClient  from "./util/queryClient";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </QueryClientProvider>
 
   </StrictMode>,
 )
